9c6822cf9fcf356c4e1ce578e429c245f5c9934d
Fbernabe1609 <fbernabe160903@gmail.com>
2023-01-17 21:10:13 +0100

    Avance

--- src/main/java/org/example/Controller/Client.java
+++ src/main/java/org/example/Controller/Client.java
@@ -1,4 +1,54 @@
 package org.example.Controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+    static Socket client;
+    static PrintWriter out;
+    static BufferedReader in;
+    static Scanner sc = new Scanner(System.in);
+    public static void start(String ip, int port) {
+        System.out.println("Cliente encendido.\nConectando con el servidor.");
+        try {
+            client = new Socket(ip, port);
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static String send() {
+        String response = "";
+        String text = "";
+        try {
+            System.out.println("Introduce un mensaje: ");
+            text = sc.nextLine();
+            System.out.println("Enviando mensaje.");
+            out.println(text);
+            response = in.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            response = "Error, no hubo mensaje.";
+        }
+        return response;
+    }
+    public static void stop() {
+        try {
+            in.close();
+            out.close();
+            client.close();
+            System.out.println("Cliente apagado.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+        start("localhost",4900);
+    }
 }
--- src/main/java/org/example/Controller/Server.java
+++ src/main/java/org/example/Controller/Server.java
@@ -1,4 +1,54 @@
 package org.example.Controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    static ServerSocket server;
+    static Socket client;
+    static PrintWriter out;
+    static BufferedReader in;
+
+    public static void start(int port) {
+        System.out.println("Iniciando servidor");
+        try {
+            server = new ServerSocket(port);
+            client = server.accept();
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String input;
+            while ((input = in.readLine()) != null) {
+                System.out.println("Mensaje del cliente: " + input);
+                if (input.equals("salir")){
+                    System.out.println("Apagando servidor");
+                    stop();
+                    break;
+                }
+
+                System.out.println("Enviando respuesta");
+                out.println(input);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Servidor apagado.");
+    }
+    public static void stop() {
+        try {
+            in.close();
+            out.close();
+            client.close();
+            server.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        start(4900);
+    }
 }
